2417316651a41a90d9f8136d664829db0ec2be4b
Divya Gupta <divygupta0319@gmail.com>
2025-07-25 18:21:53 +0530

    Login functionality working properly now, when login button clicked from book reviews page

--- src/main/java/com/fastturtle/hibernateallmappingsspringboot/configs/SecurityConfig.java
+++ src/main/java/com/fastturtle/hibernateallmappingsspringboot/configs/SecurityConfig.java
@@ -1,15 +1,22 @@
 package com.fastturtle.hibernateallmappingsspringboot.configs;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+import java.io.IOException;
 
 @Configuration
 public class SecurityConfig {
@@ -31,21 +38,42 @@ public class SecurityConfig {
                         auth.requestMatchers(HttpMethod.POST,"/booksReferred/*/bookReviews").authenticated()
                                 .anyRequest().permitAll()
                 ).formLogin(f -> f
-                        .loginPage("/login").permitAll()
+                        .loginPage("/login")
+                        .successHandler(customSuccessHandler())
+                        .permitAll()
                 ).logout(logout -> logout
                                 .logoutUrl("/logout").permitAll()
                                 .logoutSuccessUrl("/login?logout=true")
-                ).exceptionHandling(e -> e
-                        .authenticationEntryPoint((req, res, authEx) -> {
-                            if ("XMLHttpRequest".equals(req.getHeader("X-Requested-With"))) {
-                                res.sendError(HttpServletResponse.SC_FORBIDDEN);
-                            } else {
-                                res.sendRedirect("/login");
-                            }
-                        })
                 );
+//                .exceptionHandling(e -> e
+//                        .authenticationEntryPoint((req, res, authEx) -> {
+//                            if ("XMLHttpRequest".equals(req.getHeader("X-Requested-With"))) {
+//                                res.sendError(HttpServletResponse.SC_FORBIDDEN);
+//                            } else {
+//                                res.sendRedirect("/login");
+//                            }
+//                        })
+//                );
 
         return http.build();
 
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler customSuccessHandler() {
+        return (request, response, authentication) -> {
+            HttpSession session = request.getSession(false);
+            if (session != null) {
+                String redirectUrl = (String) session.getAttribute("redirectAfterLogin");
+                if (redirectUrl != null) {
+                    session.removeAttribute("redirectAfterLogin");
+                    response.sendRedirect(redirectUrl);
+                    return;
+                }
+            }
+
+            // fallback
+            response.sendRedirect("/");
+        };
     }
 }
